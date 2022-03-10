a7e74dc4a694307afdd0c8a78a83417d93a959e2
Timi-of-Earth <shodiyatimilehin@gmail.com>
2022-03-10 09:36:52 -0800

    Task 4 compiled

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-  char str[60] = "\"Programming is like building a multilingual puzzle";
+  char str1[60] = "\"Programming is like building a multilingual puzzle";
 
 
   puts(str1);
